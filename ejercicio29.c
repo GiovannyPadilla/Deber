05c6db003086fa5e52e321f3afa9d7e0f5334a83
unknown <giovanny.padilla@udla.edu.ec>
2023-10-21 18:19:32 -0500

    Uso del if para que el numero ingresado sea leido como positivo

--- ejercicio29.c
+++ ejercicio29.c
@@ -4,8 +4,12 @@ int main (){
     //Definimos las variables//
     int num;
     int primerd;
-    printf("Ingrese un numero\n");
-    scanf("%d", &num);
+        printf("Ingrese un numero\n");
+        scanf("%d", &num);
+    if (num<0)
+    {
+        num= -num;
+    }
     
     return 0;
 }
