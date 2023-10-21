e70d468ce6262a06345b461d81e1e8a80f34bce8
unknown <giovanny.padilla@udla.edu.ec>
2023-10-21 18:58:14 -0500

    uso del bucle do-while para contar los digitos que tiene el numero ingresado por el usuario

--- ejercicio20.c
+++ ejercicio20.c
@@ -6,13 +6,20 @@ int main (){
     // pide al usuario ingresar un numero
     printf("Ingrese un numero\n");
     scanf("%d",&num);
-    // uso del if en caso de que el usuario ingrese un numero negativo
+    // en caso de que el usuario ingrese un numero negativo se usa el if para cambiar el negativo a su valor absoluto
     if (num < 0)
     {
         num = -num;
     } else{
 
     }
+    //se lo usa parta contar el numero de digitos que tiene el numero ingresado por el usuario//
+    do
+    {
+        contador++;
+        num /= 10;
+    } while (num !=0);
+    printf("El numero tiene %d digitos.\n", contador);
     
     return 0;
 }
