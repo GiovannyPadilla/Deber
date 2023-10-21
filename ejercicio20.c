3dd3f16eef6d0abbd70d81052a88e5e48d2774ca
unknown <giovanny.padilla@udla.edu.ec>
2023-10-21 18:44:36 -0500

    Uso de printf en la linea 7 para pedir al usuario ingresar un numero(ejercicio 20)

--- ejercicio20.c
+++ ejercicio20.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
 int main (){
+    //se crean las variables a usar
     int num, contador = 0;
-    
+    // pide al usuario ingresar un numero
+    printf("Ingrese un numero\n");
+    scanf("%d",&num);
+
     return 0;
 }
