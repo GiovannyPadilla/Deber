07b5d42b8b3a28c8493516df30e511bdf57e6e27
unknown <giovanny.padilla@udla.edu.ec>
2023-10-21 17:12:29 -0500

    Uso de else por si la condicion if no se cumple

--- ejercicio47.c
+++ ejercicio47.c
@@ -11,10 +11,16 @@ int main (){
     if (num<0)
     {
         printf("El factorial de un numero negativo no esta definido\n");
-    }
+    }else{
+        for ( i = 1; i < num; i++)
+        {
+            factorial *= i;
+            sumfact += factorial;
+        }
+        //imprimir resultado//
+        printf("La suma de los factoriales de los numeros del 1 al %d es: %d\n",num,sumfact);
+    } 
     
-
-
     return 0;
 
 }
