3d9eff2f1d0c3a9c58b6365b7c8f67001b0c737c
unknown <giovanny.padilla@udla.edu.ec>
2023-10-21 17:06:23 -0500

    Uso del if parta verificar si el numero ingresado es negativo o no

--- ejercicio47.c
+++ ejercicio47.c
@@ -4,9 +4,16 @@ int main (){
     int num, i;
     int factorial=1;
     int sumfact=0;
-
+    //leer el numero entero desde la entrada//
     printf("Ingrese un numero\n");
     scanf("%d",&num);
+
+    if (num<0)
+    {
+        printf("El factorial de un numero negativo no esta definido\n");
+    }
+    
+
 
     return 0;
 
