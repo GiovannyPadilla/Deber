6ad1343ffe9801d020df6fbd8db24ca3d445551f
unknown <giovanny.padilla@udla.edu.ec>
2023-10-21 17:59:22 -0500

    uso del bucle for para repetir la operacion de multiplicar el numero

--- ejercicio38.c
+++ ejercicio38.c
@@ -5,6 +5,11 @@ int main (){
     //ingresar numero entero//
     printf("Ingrese un numero entero\n");
     scanf("%d", &num);
+    printf("Tabla de multiplicar del %d:\n",num);
+    //uso del bucle for para repetir la operacion de multiplicar el numero del 1 al 12
+    for(int i=1; i<=12;i++){
+        printf("%d x %d = %d\n",num, i, num*i);
+    }
 
     return 0;
 }
