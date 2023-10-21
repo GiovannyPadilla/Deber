52eaa1737f1868e0b77c43b7e16256068ad76688
unknown <giovanny.padilla@udla.edu.ec>
2023-10-21 18:50:07 -0500

    uso del if en caso de que el numero ingresado sea negativo

--- ejercicio20.c
+++ ejercicio20.c
@@ -6,6 +6,13 @@ int main (){
     // pide al usuario ingresar un numero
     printf("Ingrese un numero\n");
     scanf("%d",&num);
+    // uso del if en caso de que el usuario ingrese un numero negativo
+    if (num < 0)
+    {
+        num = -num;
+    } else{
 
+    }
+    
     return 0;
 }
