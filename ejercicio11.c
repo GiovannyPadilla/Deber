a1249acaae1d7fab0167aec38911434e97ceb4ba
unknown <giovanny.padilla@udla.edu.ec>
2023-10-21 19:21:02 -0500

    Uso de las formulas primerd=num / 10 y segundod= num % 10 para extraer el primer y segundo digito del numero entero de dos digitos

--- ejercicio11.c
+++ ejercicio11.c
@@ -3,6 +3,12 @@
 int main (){
     // creamos las variables a usar
     int num, primerd, segundod;
-    
+    //pedimos al usuario que  ingrese un numero//
+    printf("Ingrese un numero\n");
+    scanf("%d", &num);
+    //utilizamos lo siguiente para separar y extraer el primer y segundo digito de un numero entero de dos digitos
+    primerd=num / 10;
+    segundod= num % 10;
+
     return 0;
 }
