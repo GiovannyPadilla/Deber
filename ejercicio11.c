1ce5d45a7010fddbc041f40e38537696f3a5a7e1
unknown <giovanny.padilla@udla.edu.ec>
2023-10-21 19:25:51 -0500

    Uso del if para verificar que el numero ingresado es de dos digitos (Ejercicio 11)

--- ejercicio11.c
+++ ejercicio11.c
@@ -4,11 +4,16 @@ int main (){
     // creamos las variables a usar
     int num, primerd, segundod;
     //pedimos al usuario que  ingrese un numero//
-    printf("Ingrese un numero\n");
+    printf("Ingrese un numero de dos digitos\n");
     scanf("%d", &num);
     //utilizamos lo siguiente para separar y extraer el primer y segundo digito de un numero entero de dos digitos
     primerd=num / 10;
     segundod= num % 10;
-
+    // utilizamos if para verificar que el numero ingresado es de dos digitos
+    if (num >=10 && num<=99)
+    {
+        
+    }
+    
     return 0;
 }
